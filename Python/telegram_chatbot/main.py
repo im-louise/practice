483f31655d58e6f0cb0aab79275233a2f0fbac42
im-louise <whs7045@gmail.com>
2025-07-29 10:17:06 +0900

    python last

--- Python/telegram_chatbot/main.py
+++ Python/telegram_chatbot/main.py
@@ -5,11 +5,23 @@
 # 위에꺼 2개 설치
 # 설치 완료 후 아래 터미널 명령어로 서버 on
 uvicorn main:app --reload  >> main은 내가 지정한 파일명 / app는 내가 지정한 변수명
+
+http://127.0.0.1:8000/docs 이 url 접속하면 문서화 되어 있음
+
 '''
 from fastapi import FastAPI
 import random
 import requests
+from dotenv import load_dotenv
+import os
+from openai import OpenAI
+
+load_dotenv()
 app = FastAPI()
+
+@app.get('/')
+def home():
+    return {'home':'sweet home'}
 
 @app.get('/hi')
 def hi():
@@ -31,4 +43,41 @@ def gogogo():
     }
     requests.get(URL + '/sendMessage', body)
     return {'status':'gogogo'}
+
+
+def send_message(chat_id, message):
+    # .env 에서 'TELEGRAM_BOT_TOKEN'에 해당하는 값을 불러옴
+    bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
+
+    URL = f'https://api.telegram.org/bot{bot_token}'
+    body = {
+        # 사용자 chat_id 는 어디서 가져옴..?
+        'chat_id': chat_id,
+        'text': message,
+    }
+    requests.get(URL + '/sendMessage', body)
+    
+
+# /telegram 라우팅으로 텔레그램 서버가 Bot에 업데이트가 있을 경우, 우리에게 알려줌
+@app.post('/telegram')
+async def telegram(request : requests):
+    print('텔레그램에서 요청이 들어왔다!!!!')
+    
+    data = await request.json()
+    print(data)
+    sender_id = data['message']['chat']['id']
+    input_msg = data['message']['text']
+
+    client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+    
+    res = client.responses.create(
+        model='gpt-4.1-mini',
+        input=input_msg,
+        instructions='너는 초등학교 선생님이야. 모든 질문에 친절하게 설명해줘.',
+        temperature=0
+    )
+
+    send_message(sender_id, res.output_text)
+
+    return {'status': '굿'}
 
